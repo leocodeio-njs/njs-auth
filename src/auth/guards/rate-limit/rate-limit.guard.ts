dd774eb8d08faae4f5acb7d7a6c3a65e80b1996a
leocodeio <basicleo787@gmail.com>
2025-02-12 08:21:49 +0530

    Updated code for rate limiter

--- src/auth/guards/rate-limit/rate-limit.guard.ts
+++ src/auth/guards/rate-limit/rate-limit.guard.ts
@@ -4,10 +4,13 @@ import {
   ExecutionContext,
   HttpException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { RateLimiterMemory } from 'rate-limiter-flexible';
+import { RATE_LIMIT_KEY } from '../../decorator/rate-limit/rate-limit.decorator';
 
 @Injectable()
 export class IpRateLimitGuard implements CanActivate {
+  private readonly reflector: Reflector;
   private limiter = new RateLimiterMemory({
     points: 100, // Max requests
     duration: 60 * 60, // Per hour
@@ -15,6 +18,16 @@ export class IpRateLimitGuard implements CanActivate {
   });
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    // If the route is not rate limited, return true
+    const isNotRateLimited = this.reflector.get(
+      RATE_LIMIT_KEY,
+      context.getHandler(),
+    );
+    if (isNotRateLimited) {
+      return true;
+    }
+
+    // If the route is rate limited, check the rate limit
     const req = context.switchToHttp().getRequest();
     const ip = req.ip;
 
