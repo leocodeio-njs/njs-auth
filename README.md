35a12c3e2b35304f09edde13671ffdfe39585344
saiHarshaX <saiharsha9897@gmail.com>
2025-04-15 21:26:38 +0530

    feat(semantic release): implemented semantic release

--- README.md
+++ README.md
@@ -1,28 +1,17 @@
-#### Auth Module
+# tb-authorizer
+
+Auth -> API Service
+
+## Contributing
 
 ```bash
-pnpm add @leocodeio-njs/njs-auth
+pnpm install
+
 ```
 
-```typescript
-// import auth module
-import { AuthModule } from '@leocodeio-njs/njs-auth';
+Make code changes.
 
-// Add the module to the imports array of your main module
-@Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    { provide: APP_GUARD, useClass: ApiKeyGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
-  ],
-})
-export class AppModule {}
-
-// env variables
-AUTHORIZER_URL = 'https://authorizer.leocode.io';
-AUTHORIZER_API_KEY = 'your_api_key';
-X_API_KEY = 'your_api_key';
+```bash
+pnpm commit
 ```
+no specific change
---
+++ backend/src/modules/auth.module.ts
@@ -0,0 +1,42 @@
+// src/auth/auth.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+
+// Guards
+import { ApiKeyGuard } from '../auth/guards/api/api-key.guard';
+import { RolesGuard } from '../auth/guards/auth/roles.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt/acess-token-auth.strategy.guard';
+import { IpRateLimitGuard } from '../auth/guards/rate-limit/rate-limit.guard';
+
+//decorators
+import { PassAccessTokenCheck } from '../auth/decorator/jwt/passAccessTokenCheck';
+import { Roles } from '../auth/decorator/auth/roles.decorator';
+import { Public } from '../auth/decorator/api/public.decorator';
+
+// services
+import { AuthService } from './auth.service';
+import { UserRepository } from './user/domain/ports/user.repository';
+import { OTPRepository } from './otp/domain/ports/otp.repository';
+import { SessionRepository } from './session/domain/ports/session.repository';
+import { SmsService } from '../auth/services/sms.service';
+
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    ApiKeyGuard,
+    Reflector,
+    RolesGuard,
+    IpRateLimitGuard,
+    AuthService,
+
+  ],
+  exports: [
+    ApiKeyGuard,
+    Reflector,
+    RolesGuard,
+    IpRateLimitGuard,
+    AuthService,
+  ],
+})
+export class AuthModule {}
